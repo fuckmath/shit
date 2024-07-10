b723afc20da24d2638c767c75353f5cc0084000a
fuckmath <130235978+fuckmath@users.noreply.github.com>
2024-07-10 20:29:09 +0200

    Update allaah.lua

--- allaah.lua
+++ allaah.lua
@@ -1,4 +1,7 @@
+print("shit")
 local requireModule = assert(getrenv().shared.require, "Script wasn't ran on actor!")
 local weaponController = requireModule("WeaponControllerInterface")
 
 print(weaponController)
+
+
